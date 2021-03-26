4b70bfdc1ea95c401f6d8d701d2561199d129f10
Manish Verma <vermam318@gmail.com>
2021-03-26 11:01:25 +0530

    data types in rust

---
+++ rust_basics/src/dataTypes.rs
@@ -0,0 +1,20 @@
+fn main(){
+    let x = 45;  // it is a i32 or signed 32 bit integer.
+    println!("The value of x is {} which is a signed 32 bit integer.",x);
+    
+    /* in rust we can define variable of own size as in integer of 64 bits.*/
+    let y: i64 = 50; // it is a signed 64 bit integer.
+    println!("The value of y is {} which is a signed 64 bit integer.",y);
+
+    /* in rust we can define variable of own size as in integer of 64 bits.*/
+    let y: u64 = 29; // it is a unsigned or +ve 64 bit integer.
+    println!("The value of y is {} which is an usigned 64 bit integer.",y);
+    let f: f32 = 6.7; // it is a signed 64 bit float value.
+    println!("The value of f is {} which is a signed 64 bit float value.",f);
+    
+    let b: bool = false; // it is a boolean variable;
+    println!("The value of b is {} which is boolean variable.",b);
+
+}
+
+https://github.com/ManishVerma16/Rust_Programming.git
