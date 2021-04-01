3d8f768f7f4c36572c68ac61f0876caee1735354
Manish Verma <vermam318@gmail.com>
2021-04-01 16:09:04 +0530

    while loop in rust

---
+++ rust_basics/src/while_loop.rs
@@ -0,0 +1,20 @@
+fn main(){
+    let mut n = 1;
+    while n <= 50 {
+        // print multiples of 5
+        if n%5==0{
+
+            println!("n is {}", n);
+        }
+        n += 1;
+    }
+}
+
+
+/* while loop
+while n <= 50 {
+        println!("n is {}", n);
+        n += 1;
+    }
+
+*/
