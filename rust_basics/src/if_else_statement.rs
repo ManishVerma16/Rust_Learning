d10c76f62d494d9290be8a65538f0444e7fb076b
Manish Verma <vermam318@gmail.com>
2021-04-01 16:08:18 +0530

    concept of if -else

---
+++ rust_basics/src/if_else_statement.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let n = 41;
+
+    if n <40{
+        println!("less then 30");
+    }
+    else if n==40{
+        println!("equal to 40");
+    }
+    else{
+        println!("greater then 40");
+    }
+}
